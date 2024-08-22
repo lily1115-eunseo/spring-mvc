d8cbc564d0f365baab9929b68c0b15bd04806056
배은서 <lilybae1115@gmail.com>
2024-08-22 11:13:36 +0900

    controller

---
+++ src/main/java/com/sparta/springmvc/HelloController.java
@@ -0,0 +1,34 @@
+package com.sparta.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+    @GetMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "Get Method 요청";
+    }
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "POST METHOD 요청";
+    }
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "PUT METHOD 요청";
+    }
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "DELETE METHOD 요청";
+    }
+}
